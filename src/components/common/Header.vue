78565204b64bee0cb583efefdaea6fda5a35d102
Jimyu <aliyah_7@naver.com>
2025-04-08 11:36:29 +0900

    feat: 공통 헤더 컴포넌트

---
+++ src/components/common/Header.vue
@@ -0,0 +1,46 @@
+<style scoped>
+.headerContainer {
+  position: relative;
+  width: 100dvw;
+  height: 56px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+p {
+  color: black;
+  font-size: 18px;
+}
+i {
+  position: absolute;
+  left: 16px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 3.2rem;
+  width: 3.2rem;
+
+  cursor: pointer;
+}
+</style>
+
+<template>
+  <div class="headerContainer" :style="{ backgroundColor: COLORS.GREEN02 }">
+    <i @click="navigateToHome" class="fa-solid fa-chevron-left"></i>
+    <p>{{ pageName }}</p>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+import { COLORS } from '@/util/constants';
+
+defineProps(['pageName']);
+const router = useRouter();
+
+function navigateToHome() {
+  router.push('/');
+}
+</script>
