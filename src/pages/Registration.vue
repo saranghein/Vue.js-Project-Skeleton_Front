5a29d049c663db99fc645cc848c56a4446e98f43
saranghein <saranghein@gmail.com>
2025-04-08 13:32:49 +0900

    feat: 날짜기능 추가
    (#3)

--- src/pages/Registration.vue
+++ src/pages/Registration.vue
@@ -1,9 +1,13 @@
 <script setup>
 import RegistrationButton from '@/components/Registration/RegistrationButton.vue';
+import { ref } from 'vue';
+
+const selectedDate = ref('');
+const selectedTime = ref('');
 </script>
 
 <template>
-  <div class="container mt-4">
+  <div class="container-fluid mt-4">
     <div class="row align-items-center">
       <!-- 왼쪽 아이콘 -->
       <div class="col-auto">
@@ -15,21 +19,43 @@ import RegistrationButton from '@/components/Registration/RegistrationButton.vue
         <span class="registry-title">거래 등록</span>
       </div>
     </div>
-  </div>
-
-  <div class="container">
-    <!-- 버튼 컴포넌트 -->
     <div class="row">
       <div class="col mx-4">
         <RegistrationButton @change-type="onTypeChange" />
       </div>
     </div>
-  </div>
-  <div class="container">
-    <!-- 날짜 선택 -->
-    <div class="row">
-      <div class="col">
-        <span class="date-text ms-5">날짜 및 시간</span>
+    <!-- 날짜 및 시간 입력 -->
+    <div class="row mb-2">
+      <div class="col mx-4">
+        <label for="date" class="form-label ms-2">날짜</label>
+        <div class="input-group">
+          <input
+            type="date"
+            id="date"
+            class="form-control no-default-icon"
+            v-model="selectedDate"
+          />
+          <span class="input-group-text">
+            <i class="fa-solid fa-calendar-days"></i>
+          </span>
+        </div>
+      </div>
+    </div>
+
+    <div class="row mb-4">
+      <div class="col mx-4">
+        <label for="time" class="form-label ms-2">시간</label>
+        <div class="input-group">
+          <input
+            type="time"
+            id="time"
+            class="form-control no-default-icon"
+            v-model="selectedTime"
+          />
+          <span class="input-group-text">
+            <i class="fa-regular fa-clock"></i>
+          </span>
+        </div>
       </div>
     </div>
   </div>
@@ -42,5 +68,13 @@ import RegistrationButton from '@/components/Registration/RegistrationButton.vue
 .registry-title {
   font-size: 16px;
   margin: 0px auto;
+}
+
+/* 브라우저 기본 날짜/시간 아이콘 제거 */
+input[type='date'].no-default-icon::-webkit-calendar-picker-indicator,
+input[type='time'].no-default-icon::-webkit-calendar-picker-indicator {
+  opacity: 0;
+  display: none;
+  -webkit-appearance: none;
 }
 </style>
