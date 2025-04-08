d25f87bbcf1b4fd76067509ef6446f3b3c9fa51a
saranghein <saranghein@gmail.com>
2025-04-08 13:07:57 +0900

    chore:전역 스타일 수정

---
+++ src/pages/Registration.vue
@@ -0,0 +1,46 @@
+<script setup>
+import RegistrationButton from '@/components/Registration/RegistrationButton.vue';
+</script>
+
+<template>
+  <div class="container mt-4">
+    <div class="row align-items-center">
+      <!-- 왼쪽 아이콘 -->
+      <div class="col-auto">
+        <i class="fa-solid fa-chevron-left"></i>
+      </div>
+
+      <!-- 가운데 제목 -->
+      <div class="col text-center">
+        <span class="registry-title">거래 등록</span>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <!-- 버튼 컴포넌트 -->
+    <div class="row">
+      <div class="col mx-4">
+        <RegistrationButton @change-type="onTypeChange" />
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <!-- 날짜 선택 -->
+    <div class="row">
+      <div class="col">
+        <span class="date-text ms-5">날짜 및 시간</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.data-text {
+  font-size: 15px;
+}
+.registry-title {
+  font-size: 16px;
+  margin: 0px auto;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../pages/Home.vue';
+import Registration from '@/pages/Registration.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'Home',
-      component: Home,
+      name: 'Registration',
+      component: Registration,
     },
   ],
 });
