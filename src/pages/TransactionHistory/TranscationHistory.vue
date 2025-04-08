a1f24520cf313c64c89aa03d5f7282ef9e2562d9
catlover <so405@naver.com>
2025-04-08 13:14:46 +0900

    #2 [STYLE] : 거래 내역 화면 하단부 구성

---
+++ src/pages/TransactionHistory/TranscationHistory.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="transaction-list">
+    <div class="transaction-list__filter">
+      <span class="transaction-list__filter__label">최신순</span>
+      <i
+        class="fa-solid fa-angle-down icon"
+        style="color: #dedede; padding: 3px"
+      ></i>
+    </div>
+    <transaction-list />
+  </section>
+</template>
+
+<style scoped>
+.transaction-list__filter {
+  display: flex;
+  width: 100%;
+  justify-content: right;
+  margin-left: auto;
+  align-items: center;
+}
+
+.transaction-list__filter__label {
+  font-size: 12px;
+  color: #8d8d8d;
+}
+</style>
+
+<script setup>
+import TransactionList from '@/components/transactionHistory/TransactionList.vue';
+</script>
