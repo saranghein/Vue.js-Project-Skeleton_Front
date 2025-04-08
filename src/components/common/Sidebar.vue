a54e08bf2ff562b66715216a1472580fa19b19fe
urjimyu <92876819+urjimyu@users.noreply.github.com>
2025-04-08 17:06:59 +0900

    feat: 사이드바 ui 구현

---
+++ src/components/common/Sidebar.vue
@@ -0,0 +1,89 @@
+<style scoped>
+.sideBarContainer {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	width: 70dvw;
+	height: 100dvh;
+
+	background-color: white;
+}
+
+.sideBarHeader {
+	width: 100%;
+
+	display: flex;
+	justify-content: flex-start;
+
+	margin: 17px 0 27px 16px;
+}
+
+img {
+	width: 75px;
+	height: 75px;
+
+	margin-bottom: 12px;
+
+	object-fit: cover;
+	border-radius: 50%;
+}
+
+.sideBarButtons {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	gap: 28px;
+}
+
+.sideBarProfileSection {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+}
+
+p {
+	font-size: 16px;
+	cursor: pointer;
+}
+</style>
+
+<template>
+	<div class="sideBarContainer">
+		<section class="sideBarHeader">
+			<p>{{ username }}님, 안녕하세요!</p>
+		</section>
+		<section class="sideBarProfileSection">
+			<img
+				src=""
+				alt="profile-image"
+				:style="{ backgroundColor: COLORS.GRAY01 }"
+			/>
+			<div class="sideBarButtons">
+				<p @click="goToMypage">마이페이지</p>
+				<p @click="goToHome">로그아웃</p>
+			</div>
+		</section>
+	</div>
+</template>
+
+<script setup>
+import { COLORS } from "@/util/constants";
+import { defineProps } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const goToMypage = () => {
+	router.push({ name: "Mypage" });
+};
+const goToHome = () => {
+	router.push({ name: "Home" });
+};
+
+defineProps({
+	username: {
+		type: String,
+		required: true,
+	},
+});
+</script>
--- src/pages/Mypage.vue
+++ src/pages/Mypage.vue
@@ -1,9 +1,11 @@
 <style scoped></style>
 <template>
-  <!-- <Header :pageName="'마이페이지'"></Header> -->
-  <Button :bgColor="'GREEN02'" :color="'BLACK'" :name="'등록'"></Button>
+	<!-- <Header :pageName="'마이페이지'"></Header> -->
+	<Sidebar :username="'홍길동'"></Sidebar>
+	<Button :bgColor="'GREEN02'" :color="'BLACK'" :name="'등록'"></Button>
 </template>
 <script setup>
-import Header from '@/components/common/Header.vue';
-import Button from '@/components/common/Button.vue';
+import Sidebar from "@/components/common/Sidebar.vue";
+import Header from "@/components/common/Header.vue";
+import Button from "@/components/common/Button.vue";
 </script>
