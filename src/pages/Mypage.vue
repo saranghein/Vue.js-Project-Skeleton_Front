e4ee3430d09ce34aa5bb1316e2b053f91fbe8b25
Jimyu <aliyah_7@naver.com>
2025-04-09 11:52:26 +0900

    feat: mypage ui

--- src/pages/Mypage.vue
+++ src/pages/Mypage.vue
@@ -1,7 +1,99 @@
-<style scoped></style>
+<style scoped>
+.mypageContainer {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+}
+
+.mypageImgDiv {
+  position: absolute;
+  left: 50%;
+  top: 82px;
+  transform: translateX(-50%);
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 3px;
+}
+
+.nameContainer {
+  display: flex;
+  align-items: center;
+  column-gap: 5px;
+}
+
+.editIcon {
+  color: gray;
+  cursor: pointer;
+  font-size: 12px;
+}
+
+.mypageImg {
+  width: 90px;
+  height: 90px;
+
+  background-color: lightgray;
+
+  border-radius: 50% 50%;
+  border: 4px solid #55efc4;
+}
+
+.infoContainer {
+  width: 323px;
+  display: flex;
+  flex-direction: column;
+
+  padding: 0 10px 17px 10px;
+  margin-top: 160px;
+
+  background-color: white;
+}
+
+.borderline {
+  margin: 32px 0 27px 0;
+
+  border: 1px solid #d9d9d9;
+}
+</style>
 <template>
-	<Header :pageName="'마이페이지'"></Header>
+  <Header :pageName="'마이페이지'"></Header>
+  <div class="mypageImgDiv">
+    <img class="mypageImg" src="" alt="profile-img" />
+    <div class="nameContainer">
+      <span>{{ username }}</span>
+      <i class="fa-solid fa-pencil editIcon"></i>
+    </div>
+  </div>
+  <div class="mypageContainer" :style="COLORS.GRAY01">
+    <section class="infoContainer" :style="COLORS.WHITE">
+      <InfoItem
+        :icons="'fa-solid fa-cake-candles'"
+        :subject="'생년월일'"
+        :contents="'2300.01.01'"
+      />
+      <InfoItem
+        :icons="'fa-solid fa-phone'"
+        :subject="'전화번호'"
+        :contents="'010-1234-5678'"
+      />
+      <InfoItem
+        :icons="'fa-solid fa-envelope'"
+        :subject="'이메일 주소'"
+        :contents="'example@naver.com'"
+      />
+      <div class="borderline"></div>
+      <img :src="BarcodeImg" alt="barcode" />
+    </section>
+  </div>
 </template>
 <script setup>
-import Header from "@/components/common/Header.vue";
+import { COLORS } from '@/util/constants';
+import Header from '@/components/common/Header.vue';
+import InfoItem from '@/components/mypage/InfoItem.vue';
+import BarcodeImg from '@/assets/barcode.png';
+
+const username = '홍길동';
 </script>
