c0d33dc79fcb1eec0abcf59af4c8c38ceb940282
khanz0613 <khanz0613@naver.com>
2025-04-09 17:21:14 +0900

    feat: 캘린더 생성 및 UI작업

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { setupCalendar, Calendar } from 'v-calendar';
+import 'v-calendar/style.css';
 
 import App from './App.vue';
 import router from './router';
 
 const app = createApp(App);
 
+setupCalendar(app);
 app.use(createPinia());
 app.use(router);
 
