da3d4301eecfacf11cc7e49053365d34c986bb62
saranghein <saranghein@gmail.com>
2025-04-09 15:09:24 +0900

    chore:컴포넌트분리
    (#11)

---
+++ src/components/Registration/MemoInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row mb-2">
+    <div class="col-10 col-md-6 mx-auto">
+      <label for="memo" class="form-label">메모</label>
+      <div class="input-group">
+        <input
+          type="text"
+          id="memo"
+          class="form-control no-default-icon"
+          :value="modelValue"
+          @input="$emit('update:modelValue', $event.target.value)"
+          placeholder="메모를 입력해주세요"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  modelValue: String,
+});
+defineEmits(['update:modelValue']);
+</script>
