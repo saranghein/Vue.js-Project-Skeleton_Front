8f24722cc049b5aba41c458309865b736988375d
khanz0613 <khanz0613@naver.com>
2025-04-10 13:00:56 +0900

    fix: HomeHeader 수정

--- src/components/common/HomeHeader.vue
+++ src/components/common/HomeHeader.vue
@@ -1,43 +1,7 @@
-<style scoped>
-.overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  background-color: rgba(0, 0, 0, 0.5);
-  z-index: 10;
-}
-.homeHeaderContainer {
-  position: relative;
-  width: 100%;
-  height: 56px;
-  border-radius: 12px;
-
-  display: flex;
-  justify-content: left;
-  align-items: center;
-}
-p {
-  color: black;
-  font-size: 18px;
-  margin: 0;
-}
-i {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 3.2rem;
-  width: 3.2rem;
-
-  cursor: pointer;
-}
-</style>
-
 <template>
   <div class="homeHeaderContainer" :style="{ backgroundColor: COLORS.GREEN02 }">
+    <img src="@/assets/logo1.png" alt="Payday Mayday 로고" class="logo" />
     <i class="fa-solid fa-bars" @click="toggleSidebar"></i>
-    <p>PAYDAY MAYDAY</p>
   </div>
   <Sidebar
     :isActive="isSidebarOpen"
@@ -52,7 +16,7 @@ import { ref } from 'vue';
 import Sidebar from '@/components/common/Sidebar.vue';
 import { COLORS } from '@/util/constants';
 
-defineProps({ username: { type: String, required: true } }); //사이드바에 넣을 유저네임 필수요소
+defineProps({ username: { type: String, required: true } });
 
 const isSidebarOpen = ref(false);
 
@@ -60,3 +24,43 @@ function toggleSidebar() {
   isSidebarOpen.value = !isSidebarOpen.value;
 }
 </script>
+
+<style scoped>
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.4);
+  z-index: 10;
+}
+
+.homeHeaderContainer {
+  position: relative;
+  width: 100%;
+  height: 56px;
+  border-radius: 12px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 12px;
+  gap: 12px;
+}
+
+.logo {
+  height: 100px;
+  width: auto;
+  object-fit: contain;
+}
+
+i {
+  display: flex;
+  justify-content: right;
+  align-items: center;
+  height: 3.2rem;
+  width: 3.2rem;
+  cursor: pointer;
+}
+</style>
